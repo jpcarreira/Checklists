92b230014fc85b0177c6f8b69144ff089e21b16e
jpcarreira <jpmavcarreira@gmail.com>
2013-08-03 15:24:03 +0100

    added XCode project w/ portrait orientation only

---
+++ Checklists/Checklists/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Checklists
+//
+//  Created by João Carreira on 8/3/13.
+//  Copyright (c) 2013 João Carreira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
