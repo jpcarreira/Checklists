67dba6ea5c052e22ea055c7ecb5dbd4afb478934
jpcarreira <jpmavcarreira@gmail.com>
2013-08-09 22:44:50 +0100

    added AllListsViewController and manually programmed segue to connect w/ ChecklistViewController ref #1

---
+++ Checklists/Checklists/AllListsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AllListsViewController.h
+//  Checklists
+//
+//  Created by João Carreira on 8/9/13.
+//  Copyright (c) 2013 João Carreira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AllListsViewController : UITableViewController
+
+@end
---
+++ Checklists/Checklists/AllListsViewController.m
@@ -0,0 +1,116 @@
+//
+//  AllListsViewController.m
+//  Checklists
+//
+//  Created by João Carreira on 8/9/13.
+//  Copyright (c) 2013 João Carreira. All rights reserved.
+//
+
+#import "AllListsViewController.h"
+
+@interface AllListsViewController ()
+
+@end
+
+@implementation AllListsViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    if(cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"List %d", indexPath.row];
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Table view delegate
+/**
+ * this table view delegate method is invoked when we tap a row. In the ChecklistItemViewController we worked with prototype cells and a tap in a row
+ * would automatically perform the segue because the segue was hooked up with the prototype cell. In this case the segue has to be performed manually
+ * by calling performSegueWithIdentifier with the name of the segue
+ *
+ */
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self performSegueWithIdentifier:@"ShowChecklist" sender:nil];
+}
+
+@end
